a7aa37a5760f0b3d9c317a0651a56d18ee12ba6e
Rishabh-14 <77384078+Rishabh-14@users.noreply.github.com>
2023-08-28 15:27:45 +0530

    execution

--- app.py
+++ app.py
@@ -20,4 +20,20 @@ class EasyApplyBot:
             self.config = yaml.safe_load(file)
         
         self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 10)
+    
+    def login(self):
+        self.driver.get("https://www.linkedin.com/login")
         
+        username_input = self.driver.find_element(By.ID, "username")
+        password_input = self.driver.find_element(By.ID, "password")
+        
+        username_input.send_keys(self.config['username'])
+        password_input.send_keys(self.config['password'])
+        
+        password_input.send_keys(Keys.RETURN)
+
+ if __name__ == "__main__":
+    bot = EasyApplyBot('config.yaml')
+    bot.login()
+    # We'll add more methods and functionality as we go.
